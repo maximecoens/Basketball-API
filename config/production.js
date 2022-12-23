57e30052504fc536c00a50773225476fa3e6ae38
Maxime Coens <maxime.coens@student.hogent.be>
2022-12-23 15:42:46 +0100

    online zetten api fix

--- config/production.js
+++ config/production.js
@@ -4,6 +4,10 @@ module.exports = {
     level: 'info',
     disabled: false
   },
+  cors: {
+    origins: ['https://sport-applicatie.onrender.com'],
+    maxAge: 3 * 60 * 60,
+  },
   database: {
     host : 'DATABASE_HOST',
     port : 'DATABASE_PORT',
